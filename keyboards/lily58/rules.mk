2882cd2d38f5e892be3acc73f9d9b6ea62f89994
jplanow <john.planow@mlb.com>
2021-02-16 08:54:54 -0700

    Updated Lily58 default to personal keymap

--- keyboards/lily58/rules.mk
+++ keyboards/lily58/rules.mk
@@ -9,7 +9,7 @@ MCU = atmega32u4
 #   QMK DFU      qmk-dfu
 #   ATmega32A    bootloadHID
 #   ATmega328P   USBasp
-BOOTLOADER = caterina
+BOOTLOADER = qmk-dfu 
 
 # Build Options
 #   change to "no" to disable the options, or define them in the Makefile in
@@ -18,7 +18,7 @@ BOOTLOADER = caterina
 BOOTMAGIC_ENABLE = no       # Virtual DIP switch configuration
 MOUSEKEY_ENABLE = no       # Mouse keys
 EXTRAKEY_ENABLE = no       # Audio control and System control
-CONSOLE_ENABLE = no         # Console for debug
+CONSOLE_ENABLE = yes       # Console for debug
 COMMAND_ENABLE = no        # Commands for debug and configuration
 NKRO_ENABLE = no            # Nkey Rollover - if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
 BACKLIGHT_ENABLE = no      # Enable keyboard backlight functionality
