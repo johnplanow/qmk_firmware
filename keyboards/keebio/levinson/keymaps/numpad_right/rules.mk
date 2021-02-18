298a45d9228c8f2786345d5188c2a6c5f24d2bdb
jplanow <john.planow@mlb.com>
2021-02-18 10:32:51 -0700

    Create numpad_right keymap for keebio/levinson

---
+++ keyboards/keebio/levinson/keymaps/numpad_right/rules.mk
@@ -0,0 +1,6 @@
+# Build Options
+BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
+RGBLIGHT_ENABLE = yes       # Enable keyboard RGB underglow
+CONSOLE_ENABLE = yes        # Console for debug
+
+BOOTLOADER = atmel-dfu
